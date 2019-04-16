ba865a520e8d3421eada1e4721a82eafed2df8aa
Keerthyb <keerthyb@IN-Keerthyb.local>
2019-04-16 19:45:36 +0530

    Add build directory

---
+++ build/precache-manifest.509ea8377bcbc82296633d9064de321a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2dd1ad14fd102d30b5c",
+    "url": "/static/css/main.ad19f3be.chunk.css"
+  },
+  {
+    "revision": "f2dd1ad14fd102d30b5c",
+    "url": "/static/js/main.95730f72.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a220a4d46fad1957da25",
+    "url": "/static/js/2.ab92e94f.chunk.js"
+  },
+  {
+    "revision": "70f592df8685551f72188baab6927c94",
+    "url": "/index.html"
+  }
+];
